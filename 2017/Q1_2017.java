ac1bf5e9e78061529c8649ae794a5448c0e3c550
Nishal Kulkarni <kulknishu@gmail.com>
2017-06-07 11:29:24 +0530

    Add Unsolved 2017 Q1

---
+++ 2017/Q1_2017.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Q1_2017 {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N;
+
+        System.out.println("");
+        System.out.print("N=");
+        N = sc.nextInt();
+
+        try {
+            if(N<=1000){
+                
+            }else{
+                System.out.print("INVALID INPUT");
+            }            
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.print("INVALID INPUT");
+        }        
+        
+    }
+
+} 
