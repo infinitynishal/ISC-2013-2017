2935d7d976e3e371cc37f4798872e8f0fa2f3032
Nishal Kulkarni <kulknishu@gmail.com>
2017-06-07 14:32:56 +0530

    Create basic template for Q1_2016

---
+++ 2016/Q1_2016.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Q1_2016 {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N,i;
+        //Accept N as Input
+        System.out.println("");
+        System.out.print("N= ");
+        N = sc.nextInt();
+        int temp = N;
+
+        try {
+            //check basic condition
+            if(N>1){
+                        
+            }else{
+                System.out.println("INVALID INPUT");
+            }            
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("INVALID INPUT");
+        }        
+        
+    }
+
+} 
