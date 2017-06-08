7eca5233f9e089d1d9c48b6e9b938d91431ef1d8
Nishal Kulkarni <kulknishu@gmail.com>
2017-06-08 15:57:04 +0530

    Finish Q3_2016

--- 2016/Q3_2016.java
+++ 2016/Q3_2016.java
@@ -3,10 +3,11 @@ import java.util.*;
 import javax.print.DocFlavor.STRING;
 public class Q3_2016 {
     public boolean vowelChecker(String s){
-        char fc,lc;
-        int len = s.length();
+        char fc,lc;//First Char & Last Char
+        int len = s.length();//String Length
         fc = s.charAt(0);
         lc = s.charAt(len-1);
+        //Check if the string begins and ends with a vowel
         if((fc=='A'||fc=='E'||fc=='I'||fc=='O'||fc=='U')&&(lc=='A'||lc=='E'||lc=='I'||lc=='O'||lc=='U')){
             return true;
         }else{
@@ -17,6 +18,7 @@ public class Q3_2016 {
         Q3_2016 ob = new Q3_2016();
         Scanner sc = new Scanner(System.in);
         String in="",first="",second="",resent ="";
+        //Validate,counter & loop variable
         int i,count=0;
         boolean valid = false;        
         //Accepting String in UPPERCASE
@@ -38,6 +40,10 @@ public class Q3_2016 {
                 for(i=0;i<N;i++){
                     valid = false;
                     valid = ob.vowelChecker(arr[i]);
+                    /*Create 
+                    first = string consists of words with vowel at start and end
+                    last = all other words in input order
+                    */
                     if(valid==true){
                         count++;
                         first = first.concat(arr[i]+" ");
@@ -45,7 +51,9 @@ public class Q3_2016 {
                         second = second.concat(arr[i]+" ");
                     }
                 } 
+                //Join the two strings to make sentence
                 resent=first+second;
+                //OUTPUT
                 System.out.println("NUMBER OF WORDS BEGINNING AND ENDING WITH A VOWEL = "+count);    
                 System.out.println(resent);  
             }else{
