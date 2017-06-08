0e61d69a43c95f6f81370f270e0fa5d2ff40869a
Nishal Kulkarni <kulknishu@gmail.com>
2017-06-08 15:16:41 +0530

    Basic template for Q3_2016

---
+++ 2016/Q3_2016.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Q3_2016 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String in="";
+        int i;        
+        //Accepting String in UPPERCASE
+        System.out.println("");
+        in = sc.nextLine();
+        in = in.toUpperCase();
+
+        try {
+            int lastpos = in.length()-1;
+            if(in.charAt(lastpos)=='.'||in.charAt(lastpos)=='!'||in.charAt(lastpos)=='?'){
+                StringTokenizer st = new StringTokenizer(in," .?!");
+                int N = st.countTokens();
+                String arr[] = new String[N]; 
+                //Array with individual Words as elements          
+                for (i=0;i<N;i++){
+                    arr[i] = st.nextToken();
+                }
+                for (i=0;i<N;i++){
+                    System.out.println(arr[i]);
+                }         
+            }else{
+                System.out.print("INVALID INPUT");
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.print("INVALID INPUT");
+        }        
+        
+    }
+
+} 
