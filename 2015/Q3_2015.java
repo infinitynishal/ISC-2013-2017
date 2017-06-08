96acabaadcf07e802c80cba39b2297587ca820dc
Nishal Kulkarni <kulknishu@gmail.com>
2017-06-08 16:47:34 +0530

    10% progress on Q3_2015

--- 2015/Q3_2015.java
+++ 2015/Q3_2015.java
@@ -1,17 +1,19 @@
 import java.util.*;
+
+import javax.print.DocFlavor.CHAR_ARRAY;
 public class Q3_2015 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String in="";
         int i;        
-        //Accepting String in UPPERCASE
+        //Accepting String
         System.out.println("");
         in = sc.nextLine();
-        in = in.toUpperCase();
 
         try {
             int lastpos = in.length()-1;
-            if(in.charAt(lastpos)=='.'||in.charAt(lastpos)=='?'){
+            char last = in.charAt(lastpos);
+            if(last=='.'||last=='?'){
                 StringTokenizer st = new StringTokenizer(in," .?");
                 int N = st.countTokens();
                 String arr[] = new String[N]; 
@@ -19,6 +21,20 @@ public class Q3_2015 {
                 for (i=0;i<N;i++){
                     arr[i] = st.nextToken();
                 }
+                //Convert the first letter of each word to uppercase          
+                for (i=0;i<N;i++){
+                    String is = arr[i];//initial string
+                    int len = is.length();
+                    char ch = is.charAt(0);
+                    ch = Character.toUpperCase(ch);
+                    //create new string with first char in UPPERCASE
+                    String ns = (ch+"").concat(is.substring(1));
+                    arr[i]=ns;
+                }
+                //Print the changed sentence
+                for (i=0;i<N;i++){
+                    System.out.print(arr[i]+" ");
+                }System.out.print(last);
 
             }else{
                 System.out.println("INVALID INPUT");
